863353897e656e6373b7794cc5fd08ae7cf49f33
Efrosine <efrosinegames@gmail.com>
2022-09-28 02:40:12 +0700

    Pendahuluan3_2

--- src/Modul3/Pendahuluan3_1.java
+++ src/Modul3/Pendahuluan3_1.java
@@ -5,15 +5,15 @@ import java.io.*;
 public class Pendahuluan3_1 {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            
-        try {     
+
+        try {
             System.out.print("Nama : ");
             String nama = reader.readLine();
             System.out.print("Umur : ");
             String umur = reader.readLine();
-            
+
             System.out.println("\nHasil");
-            System.out.println("Nama : " +nama+"\nUmur : " + umur+" tahun");
+            System.out.println("Nama : " + nama + "\nUmur : " + umur + " tahun");
         } catch (IOException ex) {
             System.out.println("Error");
         }
