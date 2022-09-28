07de861475d839eeaaae24ca98c4ebe7d4376693
Efrosine <efrosinegames@gmail.com>
2022-09-28 12:23:47 +0700

    revisiLagi

--- src/Modul3/Pendahuluan3_4.java
+++ src/Modul3/Pendahuluan3_4.java
@@ -7,16 +7,22 @@ public class Pendahuluan3_4 {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Input Nilai a = ");
+       
         int a = 0;
+        int b = 0;
         try {
+            System.out.print("Input Nilai a = ");
             a = Integer.parseInt(reader.readLine());
+            System.out.print("Input Nilai b = ");
+            b = Integer.parseInt(reader.readLine());
         } catch (IOException ex) {
 
         }
-        int b = Integer.parseInt(JOptionPane.showInputDialog("Input Nilai B"));
+        int aa = Integer.parseInt(JOptionPane.showInputDialog("Input Nilai A"));
+        int bb = Integer.parseInt(JOptionPane.showInputDialog("Input Nilai B"));
 
         System.out.println("\nnilai a = " + a + "\tnilai b = " + b);
-        System.out.println("Hasil penjumlahan a dan b adalah = " + (a + b));
+        System.out.println("(Buffer)Hasil penjumlahan a dan b adalah = " + (a + b));
+        System.out.println("(JOption)Hasil penjumlahan aa dan bb adalah = " + (aa + bb));
     }
 }
