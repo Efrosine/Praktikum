5500028994c4082ba67a410f9e31a9b8ecd35f5c
Efrosine <efrosinegames@gmail.com>
2022-10-26 10:56:55 +0700

    revisipend6_6

--- src/Modul6/Pendahuluan6_6.java
+++ src/Modul6/Pendahuluan6_6.java
@@ -4,16 +4,17 @@ public class Pendahuluan6_6 {
     public static void main(String[] args) {
         int data2[][] = { { 4, 6, 4, 7, 8, 3, 2, 10 }, { 4, 6, 4, 2, 8, 8, 2, 10 } };
 
-        int sumArr =0,lengthArr=0;
+        int sumArr =0;
+        double element=0;
 
         for(int i =0;i<data2.length;i++){
             for(int j =0;j<data2[i].length;j++){
                 sumArr += data2[i][j];
-                lengthArr++;
+                element++;
             }
         }
 
-        int rerata = sumArr/lengthArr;
+        double rerata = sumArr/element;
         System.out.println("Nilai Reratanya : "+rerata);
     }
 }
