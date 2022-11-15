2c9c8abff1c981a1a7492c9f5422ab97c4d2a015
Efrosine <efrosinegames@gmail.com>
2022-11-15 23:45:30 +0700

    comant

--- src/Modul9/Percobaan9_2.java
+++ src/Modul9/Percobaan9_2.java
@@ -1,15 +1,21 @@
 package Modul9;
 
+//import class yang diperlukan
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class Percobaan9_2 extends JFrame implements ActionListener {
-    JTextField jp = new JTextField();
+    // Deklarasi class JTextFiled
+    JTextField textF = new JTextField();
+    // Deklarasi array JButton untuk menyimpan button yang sesuai fungsinya
     JButton[] numeric = new JButton[10], operand = new JButton[6];
-    int iNum = 0, iOp = 0, eq, n1, n2;
+    // inisialisasi var (int) untuk menyimpan hasil, value pertama, dan value kedua
+    int equal, n1, n2;
+    // Inisialisasi var (char) untuk menyimpan operator
     char op;
 
+    // Setting JFrame sesuai keinginan
     Percobaan9_2() {
         setTitle("DesignPreview");
         setSize(270, 300);
@@ -18,24 +24,25 @@ public class Percobaan9_2 extends JFrame implements ActionListener {
     }
 
     void tampil(Color bg) {
-        jp.setBounds(10, 10, 230, 40);
-        add(jp);
-        int row = 60, index = 0;
+        textF.setBounds(10, 10, 230, 40);
+        add(textF);
+        int row = 60, xString = 0, xNum = 0, xOper = 0;
         String[] s = { "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "C", "=", "/" };
         for (int i = 0; i < 4; i++) {
             int column = 10;
             for (int j = 0; j < 4; j++) {
-                JButton jb = new JButton(s[index]);
+                JButton jb = new JButton(s[xString]);
                 jb.setBounds(column, row, 50, 40);
                 jb.setBackground(bg);
                 jb.addActionListener(this);
                 add(jb);
                 column += 60;
+                //Block try catch digunakan untuk menegelompokkan button
                 try {
-                    int test = Integer.parseInt(s[index++]);
-                    numeric[iNum++] = jb;
-                } catch (Exception e) {
-                    operand[iOp++] = jb;
+                    int test = Integer.parseInt(s[xString++]);
+                    numeric[xNum++] = jb;
+                } catch (NumberFormatException e) {
+                    operand[xOper++] = jb;
                 }
             }
             row += 50;
@@ -45,17 +52,19 @@ public class Percobaan9_2 extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Deklarasi JButton yang menyimpan button yang digunakan
         JButton button = (JButton) e.getSource();
-        if (button == operand[3]) {
-            eq = n1 = n2 = 0;
-            jp.setText("");
-        } else if (button == operand[4]) {
-            n2 = Integer.parseInt(jp.getText());
+        if (button == operand[3]) { // operand3 == "C"
+            equal = n1 = n2 = 0;
+            textF.setText("");
+        } else if (button == operand[4]) { // operand4 == "="
+            n2 = Integer.parseInt(textF.getText());
             eval();
-            jp.setText(String.valueOf(eq));
-
+            textF.setText(String.valueOf(equal));
         } else {
             boolean opf = false;
+
+            // Menetapkan value op sesuai operator yang di pilih
             if (button == operand[0]) {
                 op = '+';
                 opf = true;
@@ -72,35 +81,36 @@ public class Percobaan9_2 extends JFrame implements ActionListener {
                 op = '/';
                 opf = true;
             }
-            if (opf) {
-                n1 = Integer.parseInt(jp.getText());
-                jp.setText("");
-            } else {
+
+            if (opf) { // Jika button operator ditekan value tf di tetapkan sebagai n1
+                n1 = Integer.parseInt(textF.getText());
+                textF.setText("");
+            } else { // Jika button numerik ditekan value tf akan bertambah
                 for (int i = 0; i < numeric.length; i++) {
                     if (button == numeric[i]) {
-                        String text = jp.getText();
+                        String text = textF.getText();
                         text += numeric[i].getText();
-                        jp.setText(text);
+                        textF.setText(text);
                     }
                 }
             }
         }
-
     }
 
+    //Method yang digunakan menghitung hasil perhitungan
     private void eval() {
         switch (op) {
             case '+':
-                eq = n1 + n2;
+                equal = n1 + n2;
                 break;
             case '-':
-                eq = n1 - n2;
+                equal = n1 - n2;
                 break;
             case '*':
-                eq = n1 * n2;
+                equal = n1 * n2;
                 break;
             case '/':
-                eq = n1 / n2;
+                equal = n1 / n2;
                 break;
         }
     }
